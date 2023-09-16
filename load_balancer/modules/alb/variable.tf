5a11ba9d54fa5ee54b08d796ca24a620bfd5462c
MUTHUMMK <126318277+MUTHUMMK@users.noreply.github.com>
2023-09-16 09:17:37 +0530

    variable.tf

--- load_balancer/modules/alb/variable.tf
+++ load_balancer/modules/alb/variable.tf
@@ -2,7 +2,7 @@ variable "instance_id" {}
 variable "template_tag" {}
 variable "template_instance_type" {}
 variable "template_key_name" {}
-variable "alb_sg" {}
+variable "alb sg" {}
 /*
 variable "public_subnet_cidrs" {}
 variable "as_capacity" {}
